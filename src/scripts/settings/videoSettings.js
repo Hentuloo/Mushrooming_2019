04183d1a73eda5c6f274c2b4545563eff801acd2
Kamil <Kamil@>
2019-10-08 20:00:28 +0200

    rename gifs folder to video

--- src/scripts/settings/videoSettings.js
+++ src/scripts/settings/videoSettings.js
@@ -1,142 +1,142 @@
 const videoSettings = {
     diceThrow: [
         {
-            src: './assets/gifs/diceThrow1.mp4',
+            src: './assets/videos/diceThrow1.mp4',
             alt: 'Kasyno rzut kostką',
             time: 2400,
         },
         {
-            src: './assets/gifs/diceThrow2.mp4',
+            src: './assets/videos/diceThrow2.mp4',
             alt: 'Rzut kostką',
             time: 1700,
         },
         {
-            src: './assets/gifs/diceThrow3.mp4',
+            src: './assets/videos/diceThrow3.mp4',
             alt: 'Rzut kostką',
             time: 1500,
         },
         {
-            src: './assets/gifs/diceThrow4.mp4',
+            src: './assets/videos/diceThrow4.mp4',
             alt: 'Dziecko układa kostki',
             time: 4000,
         },
     ],
     benefit: [
         {
-            src: './assets/gifs/benefit2.mp4',
+            src: './assets/videos/benefit2.mp4',
             alt: 'Szybki bieg',
             time: 3600,
         },
         {
-            src: './assets/gifs/benefit3.mp4',
+            src: './assets/videos/benefit3.mp4',
             alt: 'Mrugnięcie okiem',
             time: 1800,
         },
         {
-            src: './assets/gifs/benefit4.mp4',
+            src: './assets/videos/benefit4.mp4',
             alt: 'Zadowolony minionek',
             time: 2800,
         },
         {
-            src: './assets/gifs/universal3.mp4',
+            src: './assets/videos/universal3.mp4',
             alt: 'Conor popisówa',
             time: 3200,
         },
     ],
     wait: [
         {
-            src: './assets/gifs/wait1.mp4',
+            src: './assets/videos/wait1.mp4',
             alt: 'Sfrustrowane dziecko czeka',
             time: 1700,
         },
         {
-            src: './assets/gifs/wait2.mp4',
+            src: './assets/videos/wait2.mp4',
             alt: 'Jaś fasola dziecko czeka',
             time: 3000,
         },
         {
-            src: './assets/gifs/universal1.mp4',
+            src: './assets/videos/universal1.mp4',
             alt: 'Zdenerwoanie wobec gry',
             time: 2100,
         },
         {
-            src: './assets/gifs/obs1.mp4',
+            src: './assets/videos/obs1.mp4',
             alt: 'Zdziwiony Minionek',
             time: 2100,
         },
         {
-            src: './assets/gifs/obs4.mp4',
+            src: './assets/videos/obs4.mp4',
             alt: 'Wkurzony mężczyzna',
             time: 3000,
         },
     ],
     help: [
         {
-            src: './assets/gifs/universal2.mp4',
+            src: './assets/videos/universal2.mp4',
             alt: 'Wygrana w kasynie',
             time: 2900,
         },
         {
-            src: './assets/gifs/universal4.mp4',
+            src: './assets/videos/universal4.mp4',
             alt: 'Minionki składają gratulacje',
             time: 1400,
         },
         {
-            src: './assets/gifs/benefit3.mp4',
+            src: './assets/videos/benefit3.mp4',
             alt: 'Mrugnięcie okiem',
             time: 1800,
         },
         {
-            src: './assets/gifs/obs2.mp4',
+            src: './assets/videos/obs2.mp4',
             alt: 'Minionek alarm',
             time: 2000,
         },
     ],
     obstacle: [
         {
-            src: './assets/gifs/obs1.mp4',
+            src: './assets/videos/obs1.mp4',
             alt: 'Zdziwiony Minionek',
             time: 2100,
         },
         {
-            src: './assets/gifs/obs2.mp4',
+            src: './assets/videos/obs2.mp4',
             alt: 'Minionek alarm',
             time: 2000,
         },
         {
-            src: './assets/gifs/obs4.mp4',
+            src: './assets/videos/obs4.mp4',
             alt: 'Wkurzony mężczyzna',
             time: 3000,
         },
         {
-            src: './assets/gifs/universal1.mp4',
+            src: './assets/videos/universal1.mp4',
             alt: 'Zdenerwoanie wobec gry',
             time: 2100,
         },
     ],
     gameEnd: [
         {
-            src: './assets/gifs/universal2.mp4',
+            src: './assets/videos/universal2.mp4',
             alt: 'Wygrana w kasynie',
             time: 2900,
         },
         {
-            src: './assets/gifs/universal3.mp4',
+            src: './assets/videos/universal3.mp4',
             alt: 'Conor popisówa',
             time: 3200,
         },
         {
-            src: './assets/gifs/universal4.mp4',
+            src: './assets/videos/universal4.mp4',
             alt: 'Minionki składają gratulacje',
             time: 1400,
         },
         {
-            src: './assets/gifs/obs1.mp4',
+            src: './assets/videos/obs1.mp4',
             alt: 'Zdziwiony Minionek',
             time: 2100,
         },
         {
-            src: './assets/gifs/benefit3.mp4',
+            src: './assets/videos/benefit3.mp4',
             alt: 'Mrugnięcie okiem',
             time: 1800,
         },
